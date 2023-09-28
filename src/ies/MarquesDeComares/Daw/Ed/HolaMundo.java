f7505ea994b943f785a30fb26f19e82aa8c9bc4f
Francisco-Aidenblue2 <franciscokdragoney@gmail.com>
2023-09-28 11:27:26 +0200

    feat: I've added my name.

--- src/ies/MarquesDeComares/Daw/Ed/HolaMundo.java
+++ src/ies/MarquesDeComares/Daw/Ed/HolaMundo.java
@@ -5,6 +5,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo, soy una makina");
+		System.out.println("Soy Francisco Manuel Adamuz Vico");
 	}
 
 }

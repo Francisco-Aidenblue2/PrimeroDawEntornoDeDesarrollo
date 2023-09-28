96f6239b9c249c460d44b49f5641ea0af57cbff3
Francisco-Aidenblue2 <franciscokdragoney@gmail.com>
2023-09-28 10:50:17 +0200

    feat: I add my new repository

---
+++ src/ies/MarquesDeComares/Daw/Ed/HolaMundo.java
@@ -0,0 +1,10 @@
+package ies.MarquesDeComares.Daw.Ed;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
+	}
+
+}

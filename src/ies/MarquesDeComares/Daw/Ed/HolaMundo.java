b8d566ac9c9a80e2d7bcc64b6711f3b139896111
Eloy1989 <mantecado23@hotmail.es>
2023-09-29 14:15:38 +0200

    fix

--- src/ies/MarquesDeComares/Daw/Ed/HolaMundo.java
+++ src/ies/MarquesDeComares/Daw/Ed/HolaMundo.java
@@ -7,6 +7,7 @@ public class HolaMundo {
 		System.out.println("Hola mundo, soy una makina");
 		System.out.println("Soy Francisco Manuel Adamuz Vico");
 		System.out.println("my name is Eloy");
+		
 	}
 
 }
